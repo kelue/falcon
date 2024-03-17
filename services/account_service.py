237091c1c0337d284cc5ab759010bdefedc3cddd
kelue <austincmoon5@gmail.com>
2024-03-17 12:44:48 +0100

    Add import statement and implement get_user_demat method

--- services/account_service.py
+++ services/account_service.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from models import AccountListResponse, Account
 
 class AccountService:
@@ -16,3 +17,33 @@ class AccountService:
             return account_list_response.accountslist
         else:
             return []
+
+        #for testing
+        # return [Account(pseudoAccountName="NPG0001", fund=100000, accountId="VL580K", stoplosstype="number", stoploss=1000)]
+        
+    def get_user_demat(self, id) -> float:
+
+        api_key = os.getenv("STOCKS_DEVELOPER_API_KEY")  
+        url = "https://api.stocksdeveloper.in/trading/readPlatformMargins"
+        headers = {'api-key': api_key}
+
+        data = {'pseudoAccount': id}
+
+        res = requests.get(url, headers=headers, data=data)
+
+        if res.status == True and res.result != None:
+
+            demat_margin = 0
+
+            for margin in res.result:
+                if margin.category == "EQUITY":
+                    demat_margin += margin.funds
+                    break 
+                else:
+                    continue
+
+        return demat_margin            
+
+
+
+        
